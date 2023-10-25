4ca83c3a7a7225ed3fe4371e9e1798b79cefd47b
Vasiliy Kidyasov <kidyasovvb@gmail.com>
2023-10-25 12:17:26 +0300

    initial commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# repair-swagger
+Чиним сваггер
---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [processedJSON, setProcessedJSON] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const content = JSON.parse(e.target.result);
+        const processedContent = processJSON(content);
+        setProcessedJSON(JSON.stringify(processedContent));
+      };
+
+      reader.readAsText(file);
+    }
+  };
+
+  const processJSON = (content) => {
+    const keyList = ['IndividualEntrepreneur', 'LegalEntity', 'PhysicalPerson'];
+
+    keyList.forEach((key) => {
+      const individualEntrepreneurSchema = content.components.schemas[key];
+      const fixedIndividualEntrepreneurSchema = individualEntrepreneurSchema.allOf.find(
+        (sc) => sc["$ref"] === undefined
+      );
+      content.components.schemas[key] = {
+        ...fixedIndividualEntrepreneurSchema,
+        description: individualEntrepreneurSchema.description,
+      };
+    });
+
+    return content;
+  };
+
+  const downloadProcessedJSON = () => {
+    if (processedJSON) {
+      const blob = new Blob([processedJSON], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'processed.json';
+      a.click();
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} accept=".json" />
+      {processedJSON && (
+        <div>
+          <button onClick={downloadProcessedJSON}>Скачать обработанный файл</button>
+          <pre>{processedJSON}</pre>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
